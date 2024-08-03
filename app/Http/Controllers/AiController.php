f28afe335cf43f16564b6c11564b9c9f31fd8c62
laupwing <laupwing@gmail.com>
2024-08-03 17:50:54 +0200

    chore: Add OpenAI client and update AiController to use it

--- app/Http/Controllers/AiController.php
+++ app/Http/Controllers/AiController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use OpenAI;
 
 class AiController extends Controller
 {
     public function generate()
     {
+        $open_ai = OpenAI::client(env("OPENAI_API_KEY"));
+
         return view('ai.index');
     }
 }
