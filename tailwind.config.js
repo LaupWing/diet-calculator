7118231c69e905028eb204f2bb14a499d744931c
laupwing <laupwing@gmail.com>
2024-08-03 17:26:14 +0200

    chore: Update tailwind.config.js to remove unnecessary semicolons and trailing comma

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
-import forms from "@tailwindcss/forms";
-import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms"
+import defaultTheme from "tailwindcss/defaultTheme"
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -81,4 +81,4 @@ export default {
     },
 
     plugins: [forms, require("tailwindcss-animate")],
-};
+}
