a0b76cf1a2f1bfc7deead93cd56a1437cae4ed69
laupwing <laupwing@gmail.com>
2024-08-03 14:22:44 +0200

    feat: Add @radix-ui/react-label dependency and implement Label component

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -1,4 +1,5 @@
 import { Input } from "@/Components/ui/input"
+import { Label } from "@/Components/ui/label"
 import { Head } from "@inertiajs/react"
 
 export default function Welcome() {
@@ -6,7 +7,10 @@ export default function Welcome() {
         <>
             <Head title="Welcome" />
             <div className="h-screen w-screen flex items-center justify-center">
-                <div className="grid gap-1">
+                <div className="grid gap-1 justify-start">
+                    <Label>
+                        In how many months you want to achieve your goal
+                    </Label>
                     <Input type="number" />
                 </div>
             </div>
