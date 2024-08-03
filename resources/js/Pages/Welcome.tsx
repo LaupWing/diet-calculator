c8273a434d602c512993cdba8bd52e456c8d28cb
laupwing <laupwing@gmail.com>
2024-08-03 15:53:41 +0200

    chore: Update Welcome page layout and input components

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -17,7 +17,7 @@ export default function Welcome() {
         <>
             <Head title="Welcome" />
             <div className="h-screen w-screen flex flex-col gap-6 items-center justify-center">
-                <div className="grid gap-6">
+                <div className="grid gap-10">
                     <div className="grid gap-3">
                         <h2 className="uppercase font-bold text-sm text-slate-300">
                             Info
@@ -53,11 +53,8 @@ export default function Welcome() {
                                 </Select>
                             </div>
                         </div>
-                    </div>
-                    <div className="grid gap-1 items-start">
-                        <Label>Desired Weight</Label>
-                        <div className="flex gap-2">
-                            <Input className="w-36" type="number" />
+                        <div className="grid gap-1 items-start">
+                            <Label>Physical Activity</Label>
                             <Select
                                 value={unit}
                                 onValueChange={(e) =>
@@ -66,18 +63,61 @@ export default function Welcome() {
                                 defaultValue={unit}
                             >
                                 <SelectTrigger>
-                                    <SelectValue placeholder="Weight" />
+                                    <SelectValue placeholder="Physical Activity" />
                                 </SelectTrigger>
                                 <SelectContent>
-                                    <SelectItem value="kg">KG</SelectItem>
-                                    <SelectItem value="lbs">LBS</SelectItem>
+                                    <SelectItem value="sedentary">
+                                        Sedentary: Little or no exercise.
+                                    </SelectItem>
+                                    <SelectItem value="lightly">
+                                        Lightly active: Light exercise 1-3 days
+                                        a week.
+                                    </SelectItem>
+                                    <SelectItem value="moderately">
+                                        Moderately active: Moderate exercise 3-5
+                                        days a week.
+                                    </SelectItem>
+                                    <SelectItem value="very">
+                                        Very active: Hard exercise 6-7 days a
+                                        week.
+                                    </SelectItem>
+                                    <SelectItem value="extra">
+                                        Extra active: Very hard exercise or
+                                        physical job.
+                                    </SelectItem>
                                 </SelectContent>
                             </Select>
                         </div>
                     </div>
-                    <div className="grid gap-1 items-start">
-                        <Label>Total months to achieve goal</Label>
-                        <Input className="w-36" type="number" />
+                    <div className="grid gap-3">
+                        <h2 className="uppercase font-bold text-sm text-slate-300">
+                            Goal
+                        </h2>
+                        <div className="grid gap-1 items-start">
+                            <Label>Desired Weight</Label>
+                            <div className="flex gap-2">
+                                <Input className="w-36" type="number" />
+                                <Select
+                                    value={unit}
+                                    onValueChange={(e) =>
+                                        setUnit(e as "lbs" | "kg")
+                                    }
+                                    defaultValue={unit}
+                                >
+                                    <SelectTrigger>
+                                        <SelectValue placeholder="Weight" />
+                                    </SelectTrigger>
+                                    <SelectContent>
+                                        <SelectItem value="kg">KG</SelectItem>
+                                        <SelectItem value="lbs">LBS</SelectItem>
+                                    </SelectContent>
+                                </Select>
+                            </div>
+                        </div>
+                        <div className="grid gap-1 items-start">
+                            <Label>Total months to achieve goal</Label>
+                            <Input className="w-36" type="number" />
+                        </div>
                     </div>
                 </div>
             </div>
