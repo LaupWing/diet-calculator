bf868d3acc388e2c268a619c70b4bb0bd1039400
laupwing <laupwing@gmail.com>
2024-08-03 15:39:08 +0200

    chore: Update Welcome page with current weight input and unit select component

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -1,8 +1,18 @@
 import { Input } from "@/Components/ui/input"
 import { Label } from "@/Components/ui/label"
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/Components/ui/select"
 import { Head } from "@inertiajs/react"
+import { useState } from "react"
 
 export default function Welcome() {
+    const [unit, setUnit] = useState<"lbs" | "kg">("lbs")
+
     return (
         <>
             <Head title="Welcome" />
@@ -10,7 +20,24 @@ export default function Welcome() {
                 <div className="grid gap-4">
                     <div className="grid gap-1 items-start">
                         <Label>Current Weight</Label>
-                        <Input className="w-36" type="number" />
+                        <div className="flex gap-2">
+                            <Input className="w-36" type="number" />
+                            <Select
+                                value={unit}
+                                onValueChange={(e) =>
+                                    setUnit(e as "lbs" | "kg")
+                                }
+                                defaultValue={unit}
+                            >
+                                <SelectTrigger>
+                                    <SelectValue placeholder="Weight" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    <SelectItem value="kg">KG</SelectItem>
+                                    <SelectItem value="lbs">LBS</SelectItem>
+                                </SelectContent>
+                            </Select>
+                        </div>
                     </div>
                     <div className="grid gap-1 items-start">
                         <Label>Desired months to achieve goal</Label>
