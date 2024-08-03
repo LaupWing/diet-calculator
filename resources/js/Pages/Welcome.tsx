300c5ba549d20aa7147342620401023827f6ba75
laupwing <laupwing@gmail.com>
2024-08-03 14:19:25 +0200

    feat: Add input component for Welcome page

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -1,10 +1,15 @@
+import { Input } from "@/Components/ui/input"
 import { Head } from "@inertiajs/react"
 
 export default function Welcome() {
     return (
         <>
             <Head title="Welcome" />
-            <div className="h-screen w-screen flex items-center justify-center"></div>
+            <div className="h-screen w-screen flex items-center justify-center">
+                <div className="grid gap-1">
+                    <Input type="number" />
+                </div>
+            </div>
         </>
     )
 }
