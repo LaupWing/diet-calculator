4c2927e156adfef9cc5a9664e2d3ffb5e555c4c5
laupwing <laupwing@gmail.com>
2024-08-03 15:24:15 +0200

    updated

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -6,7 +6,11 @@ export default function Welcome() {
     return (
         <>
             <Head title="Welcome" />
-            <div className="h-screen w-screen flex items-center justify-center">
+            <div className="h-screen w-screen flex flex-col gap-6 items-center justify-center">
+                <div className="grid gap-1 items-start">
+                    <Label>Desired months to achieve goal</Label>
+                    <Input className="w-36" type="number" />
+                </div>
                 <div className="grid gap-1 items-start">
                     <Label>Desired months to achieve goal</Label>
                     <Input className="w-36" type="number" />
