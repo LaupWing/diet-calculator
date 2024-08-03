66289ccfcc6f56d87aa9aaf59b1f7fe2fa7ecd09
laupwing <laupwing@gmail.com>
2024-08-03 15:47:53 +0200

    chore: Update Welcome page with current weight and height inputs

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -17,36 +17,41 @@ export default function Welcome() {
         <>
             <Head title="Welcome" />
             <div className="h-screen w-screen flex flex-col gap-6 items-center justify-center">
-                <div className="grid gap-4">
-                    <div className="grid gap-2 grid-cols-2">
-                        <div className="grid gap-1 items-start">
-                            <Label>Age</Label>
-                            <Input className="w-36" type="number" />
+                <div className="grid gap-6">
+                    <div className="grid gap-3">
+                        <h2 className="uppercase font-bold text-sm text-slate-300">
+                            Info
+                        </h2>
+                        <div className="grid gap-2 grid-cols-2">
+                            <div className="grid gap-1 items-start">
+                                <Label>Age</Label>
+                                <Input className="w-36" type="number" />
+                            </div>
+                            <div className="grid gap-1 items-start">
+                                <Label>Height</Label>
+                                <Input className="w-36" type="number" />
+                            </div>
                         </div>
                         <div className="grid gap-1 items-start">
-                            <Label>Height</Label>
-                            <Input className="w-36" type="number" />
-                        </div>
-                    </div>
-                    <div className="grid gap-1 items-start">
-                        <Label>Current Weight</Label>
-                        <div className="flex gap-2">
-                            <Input className="w-36" type="number" />
-                            <Select
-                                value={unit}
-                                onValueChange={(e) =>
-                                    setUnit(e as "lbs" | "kg")
-                                }
-                                defaultValue={unit}
-                            >
-                                <SelectTrigger>
-                                    <SelectValue placeholder="Weight" />
-                                </SelectTrigger>
-                                <SelectContent>
-                                    <SelectItem value="kg">KG</SelectItem>
-                                    <SelectItem value="lbs">LBS</SelectItem>
-                                </SelectContent>
-                            </Select>
+                            <Label>Current Weight</Label>
+                            <div className="flex gap-2">
+                                <Input className="w-36" type="number" />
+                                <Select
+                                    value={unit}
+                                    onValueChange={(e) =>
+                                        setUnit(e as "lbs" | "kg")
+                                    }
+                                    defaultValue={unit}
+                                >
+                                    <SelectTrigger>
+                                        <SelectValue placeholder="Weight" />
+                                    </SelectTrigger>
+                                    <SelectContent>
+                                        <SelectItem value="kg">KG</SelectItem>
+                                        <SelectItem value="lbs">LBS</SelectItem>
+                                    </SelectContent>
+                                </Select>
+                            </div>
                         </div>
                     </div>
                     <div className="grid gap-1 items-start">
