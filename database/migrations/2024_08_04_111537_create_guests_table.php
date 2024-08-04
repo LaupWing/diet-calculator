7a8a8ac2bbad70b005f4ee289f7a5b70bcdf47d2
laupwing <laupwing@gmail.com>
2024-08-04 13:15:43 +0200

    refactor: Add Guest model and migration

--- database/migrations/2024_08_04_111537_create_guests_table.php
+++ database/migrations/2024_08_04_111537_create_guests_table.php
@@ -11,10 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create("submissions", function (Blueprint $table) {
+        Schema::create('guests', function (Blueprint $table) {
             $table->id();
-            $table->string("email");
-
             $table->timestamps();
         });
     }
@@ -24,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists("submissions");
+        Schema::dropIfExists('guests');
     }
 };
