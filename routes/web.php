c524676645736fce9e4b8fcb306760e0a8bda9af
laupwing <laupwing@gmail.com>
2024-08-04 13:14:27 +0200

    udpated

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ Route::get('/', function () {
 Route::post("/generate", [AiController::class, "generate"])->name("generate");
 Route::get("/generated", [AiController::class, "generated"])->name("generated");
 
+Route::post("/submit-email", function () {
+    return redirect()->back();
+})->name("submit-email");
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
