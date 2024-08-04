61169bf4f39efa80d602d040891282ab8fe5ae6d
laupwing <laupwing@gmail.com>
2024-08-04 12:24:31 +0200

    refactor: Update Welcome page form handling to set goal weight from input value

---
+++ resources/js/Pages/Generated.tsx
@@ -0,0 +1,14 @@
+import { Head, useForm, usePage } from "@inertiajs/react"
+
+export default function Welcome() {
+    const page = usePage()
+    console.log(page.props.errors)
+    console.log(page.props.flash)
+
+    return (
+        <>
+            <Head title="Welcome" />
+            <div className="h-screen w-screen flex flex-col gap-6 items-center justify-center"></div>
+        </>
+    )
+}
