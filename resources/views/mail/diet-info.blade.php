63b659adbfcbb55a28a3645371af3617d292346c
laupwing <laupwing@gmail.com>
2024-08-17 14:38:47 +0200

    feat: Update diet information email template and form submission

--- resources/views/mail/diet-info.blade.php
+++ resources/views/mail/diet-info.blade.php
@@ -1,14 +1,14 @@
 <div>
     <!-- If you do not have a consistent goal in life, you can not live it in a consistent way. - Marcus Aurelius -->
     <div>
-        <h1>Hi, {{ $data['email'] }}</h1>
+        <h1>Hi, {{ $dietInfo['email'] }}</h1>
         <p>Here is your diet information:</p>
-        <p>Calories: {{ $data['calories'] }}</p>
-        <p>Current Bodyfat: {{ $data['current_bodyfat'] }}</p>
-        <p>Goal Bodyfat: {{ $data['goal_bodyfat'] }}</p>
-        <p>Protein: {{ $data['protein'] }}</p>
+        <p>Calories: {{ $dietInfo['calories'] }}</p>
+        <p>Current Bodyfat: {{ $dietInfo['current_bodyfat'] }}</p>
+        <p>Goal Bodyfat: {{ $dietInfo['goal_bodyfat'] }}</p>
+        <p>Protein: {{ $dietInfo['protein'] }}</p>
         <h2>Meals:</h2>
-        @foreach ($data['meal_plan'] as $meal)
+        @foreach ($dietInfo['meal_plan'] as $meal)
             <p>Meal: {{ $meal['meal_type'] }}</p>
             <p>Meal: {{ $meal['recipe_name'] }}</p>
             <p>Meal: {{ $meal['calories'] }}</p>
