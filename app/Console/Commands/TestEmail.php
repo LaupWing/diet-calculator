0e5768c59a46484d1c0ff1cd4d41f0801b0a7322
laupwing <laupwing@gmail.com>
2025-06-04 19:56:13 +0200

    feat: Update TestEmail command to send SevenDayMealplan email with meal data

--- app/Console/Commands/TestEmail.php
+++ app/Console/Commands/TestEmail.php
@@ -3,7 +3,9 @@
 namespace App\Console\Commands;
 
 use App\Mail\DietInfo;
+use App\Mail\SevenDayMealplan;
 use App\Models\Guest;
+use App\Models\Meal2;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Mail;
 
@@ -28,17 +30,20 @@ class TestEmail extends Command
      */
     public function handle()
     {
-        // logger(Guest::first());
-        $guest = Guest::where("id", 2)->firstOrFail();
-        logger($guest->meals);
-        Mail::to("laupwing@gmail.com")->send(new DietInfo([
-            "email" => "laupwing@gmail.com",
-            "calories" => "1300",
-            "current_bodyfat" => "25%",
-            "goal_bodyfat" => "15%",
-            "protein" => "100g",
-            "guest_id" => 2,
-            "meal_plan" => $guest->meals,
-        ]));
+
+
+        $example = [
+            "day1" => Meal2::where("day", "day1")->get(),
+            "day2" => Meal2::where("day", "day2")->get(),
+            "day3" => Meal2::where("day", "day3")->get(),
+            "day4" => Meal2::where("day", "day4")->get(),
+            "day5" => Meal2::where("day", "day5")->get(),
+            "day6" => Meal2::where("day", "day6")->get(),
+
+        ];
+        logger($example);
+        Mail::to("laupwing@gmail.com")->send(new SevenDayMealplan(
+            $example
+        ));
     }
 }
